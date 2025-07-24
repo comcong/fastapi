e19d4c2b4fbfe81ee716345fed247667dfdbed0a
comcong <sanare7844@naver.com>
2025-07-24 14:49:29 +0900

    다시 시작

--- app/kis_invesment/socket_transaction.py
+++ app/kis_invesment/socket_transaction.py
@@ -7,65 +7,67 @@ from Crypto.Util.Padding import unpad
 from base64 import b64decode
 
 menulist = "고객ID|계좌번호|주문번호|원주문번호|매도매수구분|정정구분|주문종류|주문조건|주식단축종목코드|체결수량|체결단가|주식체결시간|거부여부|체결여부|접수여부|지점번호|주문수량|계좌명|호가조건가격|주문거래소구분|실시간체결창표시여부|필러|신용구분|신용대출일자|체결종목명40|주문가격"
-
-async def get_transaction(callback=None):
-    print('====================================')
-    print('get_transaction 실행')
-    print('====================================')
-    approval_key = get_approval_key()
-    HTS_ID = settings.KIS_HTS_ID
-    tr_type = '1'
-    if settings.KIS_USE_MOCK == True:  # 모의
-        base_url = "ws://ops.koreainvestment.com:31000"
-        tr_id = 'H0STCNI9'
-    elif settings.KIS_USE_MOCK == False: # 실전
-        base_url = "ws://ops.koreainvestment.com:21000"
-        tr_id = 'H0STCNI0'
-
-    # 서버 URL 설정 (모의/실전)
-    url = f"{base_url}/tryitout/H0STCNI0"
+class data_manager:
+    def __init__(self, add_url):
+        if settings.KIS_USE_MOCK == True:  # 모의
+            base_url = "ws://ops.koreainvestment.com:31000"
+            self.tr_id = 'H0STCNI9'
+        elif settings.KIS_USE_MOCK == False:
+            base_url = "ws://ops.koreainvestment.com:21000"  # 실전
+            self.tr_id = 'H0STCNI0'
+        self.url = base_url + add_url
 
 
-    # 요청 데이터 구성
-    senddata = req_data(approval_key, tr_id, HTS_ID, tr_type)
 
-    try:
-        async with websockets.connect(url) as ws:  # 웹소켓 연결
-            print(f"한국투자증권 WebSocket 서버에 연결되었습니다: {url}")
+    async def subscribe_transaction(self, ws):
+        print('====================================')
+        print('get_transaction 실행')
+        print('====================================')
+        approval_key = get_approval_key()
+        HTS_ID = settings.KIS_HTS_ID
+        tr_type = '1'
 
-            # 데이터 전송
-            await ws.send(json.dumps(senddata))
-            print(f"요청 데이터 전송 완료: {HTS_ID} 실시간 체결 통보 요청")
+        # 요청 데이터 구성
+        senddata = req_data(approval_key, self.tr_id, HTS_ID, tr_type)
+        ws.send(json.dumps(senddata))
 
-            # 데이터 수신 루프
-            while True:
-                try:
-                    data = (await ws.recv())       # 데이터 수신 대기
-                    print('최초 수신 데이터: ', data)
-                    try:
-                        data = json.loads(data)
-                        if 'body' in data and 'output' in data['body']:
-                            iv = data['body']['output']['iv']
-                            key = data['body']['output']['key']
-                        else:
-                            pass
-
-                    except :
-                        cipher_text = data.split('|')[3]
-                        print('암호데이터: ', cipher_text)
-                        data = aes_cbc_base64_dec(key, iv, cipher_text)
-                        print('해독데이터: ', data)
-
-
-                    # 데이터 전송
-                    if callback:
-                        await callback(data)  # 비동기 콜백은 await로 호출
-
-                except Exception as e:
-                    print(f"데이터 수신 중 오류 발생: {e}")
-                    break
-    except Exception as e:
-        print(f"웹소켓 연결 중 오류 발생: {e}")
+    # try:
+    #     async with websockets.connect(url) as ws:  # 웹소켓 연결
+    #         print(f"한국투자증권 WebSocket 서버에 연결되었습니다: {url}")
+    #
+    #         # 데이터 전송
+    #         await ws.send(json.dumps(senddata))
+    #         print(f"요청 데이터 전송 완료: {HTS_ID} 실시간 체결 통보 요청")
+    #
+    #         # 데이터 수신 루프
+    #         while True:
+    #             try:
+    #                 data = (await ws.recv())       # 데이터 수신 대기
+    #                 print('최초 수신 데이터: ', data)
+    #                 try:
+    #                     data = json.loads(data)
+    #                     if 'body' in data and 'output' in data['body']:
+    #                         iv = data['body']['output']['iv']
+    #                         key = data['body']['output']['key']
+    #                     else:
+    #                         pass
+    #
+    #                 except :
+    #                     cipher_text = data.split('|')[3]
+    #                     print('암호데이터: ', cipher_text)
+    #                     data = aes_cbc_base64_dec(key, iv, cipher_text)
+    #                     print('해독데이터: ', data)
+    #
+    #
+    #                 # 데이터 전송
+    #                 if callback:
+    #                     await callback(data)  # 비동기 콜백은 await로 호출
+    #
+    #             except Exception as e:
+    #                 print(f"데이터 수신 중 오류 발생: {e}")
+    #                 break
+    # except Exception as e:
+    #     print(f"웹소켓 연결 중 오류 발생: {e}")
 
 
 def req_data(approval_key, tr_id, HTS_ID, tr_type):
@@ -85,6 +87,10 @@ def req_data(approval_key, tr_id, HTS_ID, tr_type):
         }
     }
     return senddata
+
+
+
+
 
 # AES256 DECODE
 def aes_cbc_base64_dec(key, iv, cipher_text):
--- app/websocket/realtime_websocket.py
+++ app/websocket/realtime_websocket.py
@@ -1,7 +1,26 @@
 import json
+import websockets
 from fastapi import WebSocket, WebSocketDisconnect
 from app.kis_invesment.socket_current_price import get_stock_price
-from app.kis_invesment.socket_transaction import get_transaction
+from app.kis_invesment.socket_transaction import data_manager
+
+from app.core.config import settings
+from app.services.kis_auth import get_approval_key
+
+# if settings.KIS_USE_MOCK == True:  # 모의
+#     base_url = "ws://ops.koreainvestment.com:31000"
+#     tr_id = 'H0STCNI9'
+# elif settings.KIS_USE_MOCK == False:  # 실전
+#     base_url = "ws://ops.koreainvestment.com:21000"
+#     tr_id = 'H0STCNI0'
+# approval_key = get_approval_key()
+
+transation_url = "/tryitout/H0STCNI0"
+transation = data_manager(transation_url)
+
+
+connected_transaction:bool = False
+
 
 async def current_price_endpoint(websocket: WebSocket):
     await websocket.accept()
@@ -17,18 +36,23 @@ async def current_price_endpoint(websocket: WebSocket):
     # get_stock_price 함수에 비동기 콜백 함수를 전달하여 실행
     await get_stock_price(data_callback)
 
-
+# 실시간 체결알람 엔드포인트
 async def transaction_endpoint(websocket: WebSocket):
+    global connected_transaction
     await websocket.accept()
 
-    # 비동기 콜백 함수로 변경
-    async def data_callback(data):
-        print(f"콜백을 통해 받은 데이터: {data}")
-        if type(data) == dict:
-            await websocket.send_text(json.dumps(data , ensure_ascii=False))
-        elif type(data) == str:
-            await websocket.send_text(data)
 
-    # get_stock_price 함수에 비동기 콜백 함수를 전달하여 실행
-    await get_transaction(data_callback)
+    if connected_transaction is False:
+        async with websockets.connect(transation.url) as ws:      # 실시간 체결알람 웹소켓에 연결
+            print(f"실시간 체결알람 웹소켓에 연결됨")
+            await subscribe_transaction(ws)                       # 실시간 체결알람 웹소켓에 체결알람 구독 신청
+            connected_transaction = True
+
+            while True:
+                try:
+                    data = (await ws.recv())  # 데이터 수신 대기
+                    print('최초 수신 데이터: ', data)
+                except:
+                    pass
+
 
