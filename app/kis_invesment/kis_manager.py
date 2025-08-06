cd0d0f6f073fc959e9feb5e24b1dfef1e25dd200
comcong <sanare7844@naver.com>
2025-08-06 14:50:57 +0900

    68

--- app/kis_invesment/kis_manager.py
+++ app/kis_invesment/kis_manager.py
@@ -16,7 +16,10 @@ class kis_api:
         self.__key = None
         self.__price_menulist = '종목코드|체결시간|새현재가|전일대비부호|전일대비|전일대비율|가중평균주식가격|시가|최고가|최저가|매도호가1|매수호가1|체결거래량|누적거래량|누적거래대금|매도체결건수|매수체결건수|순매수체결건수|체결강도|총매도수량|총매수수량|체결구분|매수비율|전일거래량대비등락율|시가시간|시가대비구분|시가대비|최고가시간|고가대비구분|고가대비|최저가시간|저가대비구분|저가대비|영업일자|신장운영구분코드|거래정지여부|매도호가잔량|매수호가잔량|총매도호가잔량|총매수호가잔량|거래량회전율|전일동시간누적거래량|전일동시간누적거래량비율|시간구분코드|임의종료구분코드|정적VI발동기준가'
         self.__trans_menulist = '고객ID|계좌번호|주문번호|원주문번호|매도매수구분|정정구분|주문종류|주문조건|종목코드|체결수량|체결단가|체결시간|거부여부|체결여부|접수여부|지점번호|주문수량|계좌명|호가조건가격|주문거래소구분|실시간체결창표시여부|종목명|필러'
+                               # 고객ID|계좌번호|주문번호|원주문번호|매도매수구분|정정구분|주문종류|주문조건|종목코드|체결수량|체결단가|체결시간|거부여부|체결여부|접수여부|지점번호|주문수량|계좌명|호가조건가격|주문거래소구분|실시간체결창표시여부|필러|신용구분|신용대출일자|종목명|주문가격
+                    # sanare78^5014279001^0000001562^^02^0^00^0^027360^0000000001^000002200^092701^                             0^1^1^00950^000000001^                 신명진^1Y^10^^아주IB투자^
         self.url = settings.url
+
 
 
     # ============================================================= #
@@ -38,26 +41,25 @@ class kis_api:
                 'len_data': len_data,
                 'data': data
             }
-            print('문자열에서 딕셔너리로 변환된 데이터', extracted_data)
+            # print('문자열에서 딕셔너리로 변환된 데이터', extracted_data)
 
             if extracted_data['encrypted'] == '1':        # 암호화된 데이터인 경우
-                print('추적1')
                 extracted_data['data'] = self.__aes_cbc_base64_dec(data)  # 데이터 복호화
 
             # if (extracted_data['tr_id'] == 'H0STCNI0') or (extracted_data['tr_id'] == 'H0STCNI9'):  # 실시간 체결통보
             if extracted_data['tr_id'] in ['H0STCNI0', 'H0STCNI9']:  # 실시간 체결통보
-                print('추적2')
-                print('문자열이 실시간 체결통보')
+                # print('추적2')
+                # print('문자열이 실시간 체결통보')
                 data_keys = self.__trans_menulist.split('|')
-                print('data_keys', data_keys)
-                print('key 개수: ', len(data_keys))
+                # print('data_keys', data_keys)
+                # print('key 개수: ', len(data_keys))
                 data_values = extracted_data['data'].split('^')
-                print('data_values', data_values)
-                print('value 개수: ', len(data_values))
+                # print('data_values', data_values)
+                # print('value 개수: ', len(data_values))
                 data = dict(zip(data_keys, data_values))  # zip으로 묶어서 딕셔너리 형태로 변환
                 data['tr_id'] = extracted_data['tr_id']   # data 에 tr_id 값 추가; 데이터 종류 구분하기 위해
                 df = pd.DataFrame([data])
-                print('추적3', df)
+                # print('추적3', df)
                 return df  # 완성된 현재가 데이터 df 로 리턴
 
                 # # ======== 매수 체결시  df, DB 에 데이터 넣는 자리 ==============
@@ -76,53 +78,44 @@ class kis_api:
 
 
             elif extracted_data['tr_id'] == 'H0STCNT0':    # 실시간 현재가  H0STCNT0
-                print('추적5')
-                print('문자열이 실시간 현재가', extracted_data['data'])
+                # print('추적5')
+                # print('문자열이 실시간 현재가', extracted_data['data'])
                 data_keys = self.__price_menulist.split('|')
                 data_values = data.split('^')
                 data = dict(zip(data_keys, data_values))  # zip으로 묶어서 딕셔너리 형태로 변환
                 data['tr_id'] = extracted_data['tr_id']   # data 에 tr_id 값 추가; 데이터 종류 구분하기 위해
                 df = pd.DataFrame([data])
-                print('추적10', df)
+                # print('추적10', df)
                 return df   # 완성된 현재가 데이터 df 로 리턴
 
         except:                                                   # 딕셔너리 형태의 문자열인 경우
             data = json.loads(row_data)
-            print('추적6')
+            # print('추적6')
             tr_id = data['header']['tr_id']
             if tr_id != 'PINGPONG': # PINGPONG 데이터가 아닌 경우
-                print('추적7')
+                # print('추적7')
                 rt_cd = data['body']['rt_cd']
 
                 if rt_cd == '0':                                  # 정상 데이터인 경우
-                    print('추적8')
+                    # print('추적8')
                     self.__iv = data["body"]["output"]["iv"]      # iv 값 할당
                     self.__key = data["body"]["output"]["key"]    # key 값 할당
                     msg = data["body"]["msg1"]
                     msg_cd = data["body"]["msg_cd"]
                     return data  # 정상 데이터 리턴
                 else:
-                    print('추적9')
+                    # print('추적9')
                     return data  # 비정상 데이터 리턴
 
             else:
-                print('추적10')
+                # print('추적10')
                 return data   # PINGPONG 데이터 그대로 리턴
 
 
 
-    async def buy_update(self, ws, jango_df, trans_df, code_list):
+    async def buy_update(self, ws, jango_df, trans_df):
         print('buy_update() 실행')
-        # print('jango_df: ', jango_df)
-        # print()
-        # print('trans_df: ', trans_df)
-        # print()
         ord_num = trans_df['주문번호'].values[0]
-        # print('trans_df 주문번호: ', ord_num)
-        # print('잔고주문번호리스트: ', jango_df['주문번호'].values)
-        #
-        # print('trans_df 체결시간: ', trans_df['체결시간'].values[0])
-
 
         # 주문번호가 이미 존재하는지 확인
         if ord_num in jango_df['주문번호'].values:
@@ -141,32 +134,21 @@ class kis_api:
             jango_df.at[idx, '체결단가'] = trans_df['체결단가'].values[0]
             jango_df.at[idx, '체결시간'] = trans_df['체결시간'].values[0]
 
-            # 1000 주 주문하여 쪼개져 체결될 때, json 직렬화에 실패했다고 에러 뜨면 적용 해 봐!! # NaN → None 처리
-            # jango_df = jango_df.where(pd.notnull(jango_df), None)
-
             print('buy_update() 기존 주문이 있는 경우 실행 완료')
             return jango_df
-
-            # # DB 업데이트도 필요하다면 별도 update 함수 사용
-            # kis_db.update_data(주문번호, {
-            #     '체결수량': str(기존_수량 + 신규_수량),
-            #     '체결단가': data['체결단가'],
-            #     '체결시간': data['체결시간']
-            # })
 
         else:  # 새로운 주문번호라면, 새로운 행에 추가
             print('주문번호가 없는 경우')
             tr_id = 'H0STCNT0'
             tran_code = trans_df['종목코드'].values[0]
-            jango_df = pd.concat([jango_df, trans_df], ignore_index=True)
-
+            code_list = jango_df['종목코드'].unique().tolist()
+            print('tran_code', tran_code)
+            print('code_list', code_list)
             if tran_code not in code_list:  # 새로운 종목 구독 추가
+                print('새로운 종목코드 구독 추가')
                 await self.subscribe(ws=ws, tr_id=tr_id, code_list=[tran_code])
-
-            # kis_db.insert_data(data)
-            # print('jango_df:')
-            # print(jango_df)
-            print('buy_update() 기존 주문이 없는 경우 실행 완료')
+            jango_df = pd.concat([jango_df, trans_df], ignore_index=True)
+            print('buy_update() 주문이 없는 경우 실행 완료')
             return jango_df
 
 
@@ -211,11 +193,8 @@ class kis_api:
 
     # AES256 DECODE
     def __aes_cbc_base64_dec(self, cipher_text):
-        print('암호화된 데이터: ', cipher_text)
         cipher = AES.new(self.__key.encode('utf-8'), AES.MODE_CBC, self.__iv.encode('utf-8'))
-        # data = bytes.decode(unpad(cipher.decrypt(b64decode(cipher_text)), AES.block_size))
         data = unpad(cipher.decrypt(b64decode(cipher_text)), AES.block_size).decode('utf-8')
-        print('복호화된 데이터: ', data)
         return data
 
 
@@ -236,11 +215,6 @@ class kis_api:
     # ============================================================= #
 
     async def subscribe(self, ws, tr_id=settings.tr_id_transaction, tr_type='1', code_list=None):
-        print('====================================')
-        print('kis_api.subscribe 함수 실행')
-        print('====================================')
-
-
         if tr_id in ['H0STCNI0', 'H0STCNI9']:                                               # 실시간 체결알람 tr_id
             senddata = self.__req_data(tr_id=tr_id, tr_key=self.__HTS_ID, tr_type=tr_type)  # ws 에 전송할 데이터 포맷
             await ws.send(json.dumps(senddata))                                             # 실시간 체결알람 한투api에 구독 등록 요청
@@ -251,6 +225,7 @@ class kis_api:
                 senddata = self.__req_data(tr_id=tr_id, tr_key=tr_key, tr_type=tr_type)
                 await ws.send(json.dumps(senddata))                                          # 실시간 현재가 한투api에 구독 등록 요청
                 print('실시간 현재가 등록 데이터 전송', senddata)
+        print('kis_api.subscribe 함수 실행됨')
 
     def __req_data(self, tr_id  ,tr_key, tr_type):                    # ws에 전송할 데이터 포맷
 
--- app/kis_receiver.py
+++ app/kis_receiver.py
@@ -12,28 +12,51 @@ from app.kis_invesment.kis_manager import kis
 jango_df: pd.DataFrame = pd.DataFrame()
 
 async def start_kis_receiver():
-
     global jango_df
     jango_df = jango_db()
     code_list = jango_df['종목코드'].unique().tolist() # DB 에서 종목코드 가져옴
     async with websockets.connect(kis.url) as ws:
         await kis.subscribe(ws=ws)
+        print('첫 code_list', code_list)
         await kis.subscribe(ws=ws, tr_id='H0STCNT0', code_list=code_list)
 
         while True:
             try:
                 raw_data = await ws.recv()  # ws로부터 데이터 수신
-                print("수신된 원본 데이터: ")
-                print(raw_data)
+                # print("수신된 원본 데이터: ")
+                # print(raw_data)
                 data = await kis.make_data(raw_data)  # 데이터 가공
                 print("수신된 가공 데이터: ")
                 print(data)
 
                 if isinstance(data, pd.DataFrame):
-                    json_data = data.to_dict(orient="records")
-                    data = {"type": "stock_data", "data": json_data}
-                    await websocket_manager.manager.broadcast(json.dumps(data))
-                    print('데이터프레임 전송완료')
+                    tr_id = data.iloc[0]['tr_id']
+                    if tr_id == 'H0STCNT0':  # 실시간 현재가가 들어오는 경우
+                        print('tr_id == "H0STCNT0":')
+
+                        jango_df = update_jango_df(data[['종목코드', '새현재가']].copy())
+                        json_data = strip_zeros(jango_df.to_dict(orient="records"))
+                        data = {"type": "stock_data", "data": json_data}
+                        await websocket_manager.manager.broadcast(json.dumps(data))
+                        print('데이터프레임 전송완료')
+
+                    elif (tr_id in ['H0STCNI9', 'H0STCNI0']) and (data['체결여부'].values.tolist()[0] == '2'):  # 체결통보 데이터
+                        # sanare78^5014279001^0000001562^^02^0^00^0^027360^0000000001^000002200^092701^0^1^1^00950^000000001^신명진^1Y^10^^아주IB투자^
+                        trans_df = data.copy()
+                        print('체결통보 df')
+                        print(jango_df)
+                        if trans_df['매도매수구분'].values[0] == '02':    # 01: 매도, 02: 매수
+                            jango_df = await kis.buy_update(ws=ws, jango_df=jango_df, trans_df=trans_df)
+
+                        elif trans_df['매도매수구분'].values[0] == '01':    # 01: 매도, 02: 매수
+                            jango_df = kis.sell_update(jango_df=jango_df, trans_df=trans_df)
+
+
+                        json_data = strip_zeros(jango_df.to_dict(orient="records"))
+                        print('json_data', json_data)
+                        data = {"type": "stock_data", "data": json_data}
+                        await websocket_manager.manager.broadcast(json.dumps(data))
+                        print('데이터프레임 전송완료')
 
                 else:
                     msg_data = {"type": "message", "data": data}
@@ -60,3 +83,39 @@ async def send_initial_data(websocket):
     jango_json_data = jango_df.to_dict(orient="records")
     stock_data = {"type": "stock_data", "data": jango_json_data}
     await websocket.send_text(json.dumps(stock_data))
+
+# 숫자 앞 0을 없애주는 함수
+def strip_zeros(json_list: list[dict]) -> list[dict]:
+    # print('strip_zeros 실행')
+    # print('json_list: ', json_list)
+    keys_to_strip_zeros = ['주문번호', '체결수량', '체결단가']
+    for record in json_list:
+        for key in keys_to_strip_zeros:
+            if key in record and record[key]:
+                try:
+                    record[key] = str(int(record[key]))
+                except (ValueError, TypeError):
+                    pass  # 숫자 변환 불가능한 값은 건너뜀
+    return json_list
+
+def update_jango_df(df: pd.DataFrame = None) -> pd.DataFrame:
+    # print('update_jango_df() 실행')
+    # print('update_jango_df() df')
+    # print(df)
+    global jango_df  # 실시간 현재가 데이터 전역변수 사용
+    if df is None:
+        return jango_df
+    else:
+        print('타입비교')
+        print('현재가', jango_df["현재가"])
+        print('새현재가', df["새현재가"])
+        jango_df = pd.merge(jango_df, df, on='종목코드', how='left')  # 병합
+        print('타입비교')
+        print('현재가', jango_df["현재가"])
+        print('새현재가', jango_df["새현재가"])
+        jango_df.loc[jango_df["새현재가"].notna(), "현재가"] = jango_df["새현재가"]
+        jango_df = jango_df.drop(columns=['새현재가'])
+        jango_df = jango_df[['주문번호', '종목명', '종목코드', '체결시간', '체결수량', '체결단가', '현재가']]
+        # print('update_jango_df() jango_df')
+        # print(jango_df)
+        return jango_df
