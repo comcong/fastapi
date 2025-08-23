becefc84a05615b44b94f5242fe09df46711c651
my_house <sanare7844@naver.com>
2025-08-23 20:30:25 +0900

    113

--- app/db/kis_db.py
+++ app/db/kis_db.py
@@ -1,9 +1,7 @@
 from supabase import create_client, Client
 from app.core.config import settings
-import pandas as pd
 import random
-import os, traceback, datetime as dt
-
+import traceback
 url: str = settings.SUPABASE_URL
 key: str = settings.SUPABASE_KEY
 supabase: Client = create_client(url, key)
